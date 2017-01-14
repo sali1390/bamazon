15852a96bec4282fce545a51ea688cd647babd65
Saleema Ali <sali1390@gmail.com>
2017-01-13 20:48:40 -0500

    Updated app to take user's inputs and update products database. Also provides user total cost of transaction

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,93 @@ function displayProducts() {
             console.log(res[i].item_id + ": " + res[i].product_name + " - " + res[i].price);
             console.log("---------------------------------------------");
         }
+        
+        askQuestion1();
     })
 }
 
+function askQuestion1(){
+	inquirer.prompt({
+		type: 'input',
+        name: 'item_id',
+        message: 'Please enter the id of the product you would like to buy:'
+	}).then(function(answers){
+        connection.query('SELECT * FROM products WHERE ?',{
+			item_id: answers.item_id
+		}, function(err, res, fields) {
+            if (err) throw err;
+//            console.log(res[0].product_name);
+            askQuestion2(res[0].item_id, res[0].product_name);
+        })
+//		connection.end();       
+	})
+}
+
+function askQuestion2(id, productName){
+    inquirer.prompt({
+		type: 'input',
+        name: 'quantity_requested',
+        message: 'Please enter how many you would like:'
+	}).then(function(answers){
+        connection.query('SELECT * FROM products WHERE ?',{
+			item_id: id
+		}, function(err, res, fields) {
+            if (err) throw err;
+            
+            var qRequested = answers.quantity_requested;
+            
+//            console.log(qRequested);
+            
+            checkInventory(qRequested, productName);
+        })
+        
+	})
+}
+
+function checkInventory(qReq, item){
+    connection.query('SELECT stock_quantity FROM products WHERE ?',{
+        product_name: item
+    }, function(err, res) {
+//        console.log(res[0].stock_quantity);
+        
+        if (qReq < res[0].stock_quantity){
+            connection.query('UPDATE products SET ? WHERE ?',[{
+                stock_quantity: res[0].stock_quantity-qReq
+            },{product_name: item
+              }], function(err, res, fields) {
+            if (err) throw err;
+                console.log(res);
+                
+            totalCost(item, qReq);
+            })
+        } else {
+            console.log("Insufficient quantity!");
+        }
+        
+//        console.log("It works");
+    });
+}
+
+function totalCost(item, qReq){
+    connection.query('SELECT price FROM products WHERE ?',{
+        product_name: item
+    }, function(err, res) {
+        var total = res[0].price * qReq;
+        
+        console.log("Your total today is: " + total);
+    });
+}
+
 displayProducts();
+
+
+
+
+
+
+
+
+
+
+
+
