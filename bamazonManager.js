00327d4c0ce69dc78a042a8524771d339d8abe18
Saleema Ali <sali1390@gmail.com>
2017-01-13 21:23:16 -0500

    Added functionality to view items with low stock

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,7 +13,7 @@ function menuOptions(){
 		type: 'list',
         name: 'menu_select',
         message: 'Please choose a selection from the following:',
-        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit']
 	}).then(function(answers){
         switch(answers.menu_select){
             case 'View Products for Sale':
@@ -28,10 +28,14 @@ function menuOptions(){
             case 'Add New Product':
                 addNewProduct();
             break;
+            case 'Exit':
+                exit();
+            break;
         }
 	})
 }
 
+//Function to view all products available
 function viewProducts(){
     connection.query('SELECT item_id, product_name, price FROM products', function(err, res) {
         if (err) throw err;
@@ -40,21 +44,34 @@ function viewProducts(){
             console.log(res[i].item_id + ": " + res[i].product_name + " - " + res[i].price);
             console.log("---------------------------------------------");
         }
+        menuOptions();
     });
-    
-    console.log("Hello : View Products for Sale");
 }
 
 function viewLowInventory(){
-    console.log("Hello : View Low Inventory");
+    connection.query('SELECT item_id, product_name, price, stock_quantity FROM products', function(err, res) {
+        if (err) throw err;
+        
+        for (i=0; i<res.length; i++){
+            if (res[i].stock_quantity < 5){
+                console.log(res[i].item_id + ": " + res[i].product_name + " - " + res[i].price);
+                console.log("---------------------------------------------");
+            }
+        }
+        menuOptions();
+    });
 }
 
 function addToInventory(){
-    console.log("Hello : Add to Inventory");
+    
 }
 
 function addNewProduct(){
     console.log("Hello : Add New Product");
+}
+
+function exit(){
+    connection.end();
 }
 
 menuOptions();
